1d2228e66a47683e8cdd8718565fadcdfa39c0a9
Si Thu Win <sthuwin@gmail.com>
2023-05-29 19:05:57 +1000

    duck class

---
+++ src/main/java/com/design/pattern/Duck.java
@@ -0,0 +1,13 @@
+package com.design.pattern;
+
+public class Duck {
+  void quack() {
+    System.out.println("Quack");
+  }
+  void swim(){
+    System.out.println("swim");
+  }
+  void display(){
+    System.out.println("display");
+  }
+}
